45b3d50378f7aa9956f615cedb2c24dd9f427b30
FRIB0II <nicolasfigueiredo381@gmail.com>
2024-05-26 23:36:38 -0300

    Site iniciado.

--- Pages/Sobre.cshtml.cs
+++ Pages/Sobre.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Livro_Teca.Pages
 {
-    public class PrivacyModel : PageModel
+    public class SobreModel : PageModel
     {
         public void OnGet()
         {
